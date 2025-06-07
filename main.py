ca9c21782e404430ac2dfe92a569db070a7c40f5
juliawarpas <julia.warpas@student.wat.edu.pl>
2025-06-07 23:35:14 +0200

    Stworzenie klasy klientów

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from click import command
 
 facilities = []
 employees = []
+clients = []
 
 class Facility:
     def __init__(self, name, surname, location, post, map_widget):
@@ -49,6 +50,26 @@ class Employee:
                 float(response_html.select('.longitude')[1].text.replace(',', '.'))
             ]
 
+class Client:
+    def __init__(self, name, surname, location, post, map_widget):
+        self.name = name
+        self.surname = surname
+        self.location = location
+        self.post = post
+        self.cordinates = self.get_cordinates()
+        self.marker = map_widget.set_marker(self.cordinates[0], self.cordinates[1], text=f'{self.name} {self.surname} {self.location}')
+
+    def get_cordinates(self) -> list:
+        from bs4 import BeautifulSoup
+        import requests
+        adress_url = f'https://pl.wikipedia.org/wiki/{self.location}'
+        response = requests.get(adress_url)
+        if response.status_code == 200:
+            response_html = BeautifulSoup(response.content, 'html.parser')
+            return [
+                float(response_html.select('.latitude')[1].text.replace(',', '.')),
+                float(response_html.select('.longitude')[1].text.replace(',', '.'))
+            ]
 
 def add_facilities() -> None:
     name = entry_imie.get()
@@ -216,6 +237,91 @@ def show_employee_details():
     map_widget.set_zoom(15)
     map_widget.set_position(employees[i].cordinates[0], employees[i].cordinates[1])
 
+
+def add_clients() -> None:
+    name = entry_imie.get()
+    surname = entry_nazwisko.get()
+    location = entry_miejscowosc.get()
+    post = entry_post.get()
+
+    client = clients(name=name, surname=surname, location=location, post=post, map_widget=map_widget)
+    clients.append(client)
+
+
+    print(clients)
+
+
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_post.delete(0, END)
+
+    entry_imie.focus()
+    show_clients()
+
+def show_clients() -> None:
+    listbox_lista_obiektow.delete(0, END)
+    for idx,user in enumerate(clients):
+        listbox_lista_obiektow.insert(idx, f'{idx+1}. {clients.name} {clients.surname}')
+
+def remove_client() -> None:
+    i=listbox_lista_obiektow.index(ACTIVE)
+    clients[i].marker.delete()
+    clients.pop(i)
+    show_clients()
+
+
+def edit_clients() -> None:
+    i = listbox_lista_obiektow.index(ACTIVE)
+    name = clients[i].name
+    surname = clients[i].surname
+    location = clients[i].location
+    post = clients[i].post
+
+    entry_imie.insert(0, name)
+    entry_nazwisko.insert(0, surname)
+    entry_miejscowosc.insert(0, location)
+    entry_post.insert(0, post)
+
+    button_dodaj_obiekt.configure(text='Zapisz', command=lambda: update_client(i))
+
+def update_client(i):
+    name = entry_imie.get()
+    surname = entry_nazwisko.get()
+    location = entry_miejscowosc.get()
+    post = entry_post.get()
+
+    clients[i].name = name
+    clients[i].surname = surname
+    clients[i].location = location
+    clients[i].post = post
+
+    clients[i].cordinates = clients[i].get_cordinates()
+    clients[i].marker.delete()
+    clients[i].marker = map_widget.set_marker(clients[i].cordinates[0], clients[i].cordinates[1], clients=f'{clients[i].name}')
+
+    show_clients()
+
+    button_dodaj_obiekt.config(text='Dodaj', command=add_clients())
+
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_post.delete(0, END)
+
+    entry_imie.focus()
+
+def show_client_details():
+    i=listbox_lista_obiektow.index(ACTIVE)
+    label_szczegoly_obiektu_name_wartosc.config(text=clients[i].name)
+    label_szczegoly_obiektu_surname_wartosc.config(text=clients[i].surname)
+    label_szczegoly_obiektu_miejscowosc_wartosc.config(text=clients[i].location)
+    label_szczegoly_obiektu_post_wartosc.config(text=clients[i].post)
+
+    map_widget.set_zoom(15)
+    map_widget.set_position(clients[i].cordinates[0], clients[i].cordinates[1])
+
+
 #GUI
 root = Tk()
 root.geometry("1200x700")
@@ -227,12 +333,15 @@ ramka_formularz=Frame(root)
 ramka_szczegoly_obiektu=Frame(root)
 ramka_mapa=Frame(root)
 ramka_lista_pracownikow=Frame(root)
+ramka_lista_klientow=Frame(root)
 
-ramka_lista_placowek.grid(row=0, column=0)
-ramka_formularz.grid(row=1, column=1, sticky=N)
-ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=2)
-ramka_mapa.grid(row=2, column=0, columnspan=2)
+ramka_lista_placowek.grid(row=0, column=0, sticky=N)
+ramka_formularz.grid(row=1, column=0,columnspan=3, sticky=N)
+ramka_szczegoly_obiektu.grid(row=2, column=0, columnspan=3)
+ramka_mapa.grid(row=3, column=0, columnspan=3)
 ramka_lista_pracownikow.grid(row=0, column=1, sticky=N)
+ramka_formularz.grid(row=1, column=0, columnspan=3, sticky=N)
+ramka_lista_klientow.grid(row=0, column=2, sticky=N)
 
 # ramka_lista_obiektow
 label_lista_obiektow=Label(ramka_lista_placowek, text='Lista placówek:')
@@ -271,6 +380,25 @@ button_usun_pracownika.grid(row=2, column=3)
 
 button_edytuj_pracownika=Button(ramka_lista_pracownikow, text='Edytuj', command=edit_employee)
 button_edytuj_pracownika.grid(row=2, column=4)
+
+#ramka_lista_klientów
+label_ramka_lista_klientow = Label(ramka_lista_klientow, text="Klienci:")
+label_ramka_lista_klientow.grid(row=0, column=0)
+
+listbox_ramka_lista_klientow=Listbox(ramka_lista_klientow, width=50, height=10)
+listbox_ramka_lista_klientow.grid(row=1, column=0, columnspan=3)
+
+
+button_pokaz_szczegoly_klientow=Button(ramka_lista_klientow, text='Pokaz szczegóły', command=show_client_details)
+button_pokaz_szczegoly_klientow.grid(row=2, column=0)
+
+
+button_usun_klienta=Button(ramka_lista_klientow, text='Usuń', command=remove_client)
+button_usun_klienta.grid(row=2, column=1)
+
+
+button_edytuj_klienta=Button(ramka_lista_klientow, text='Edytuj', command=edit_clients)
+button_edytuj_klienta.grid(row=2, column=2)
 
 # ramka_formularz
 label_formularz=Label(ramka_formularz, text='Formularz:')
