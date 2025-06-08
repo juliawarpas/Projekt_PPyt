4bbccdbc342549833831e4d76108293398c7e31d
juliawarpas <julia.warpas@student.wat.edu.pl>
2025-06-08 21:17:28 +0200

    edycja formatki dla placówek

--- main.py
+++ main.py
@@ -73,25 +73,31 @@ class Client:
 
 def add_object():
     name = entry_imie.get()
-    surname = entry_nazwisko.get()
     location = entry_miejscowosc.get()
     post = entry_post.get()
     typ = typ_var.get()
 
-    if not all([name, surname, location, post]):
-        print("Wszystkie pola muszą być wypełnione.")
-        return
 
     try:
         if typ == "Placówka":
-            obj = Facility(name, surname, location, post, map_widget)
+            surname = ""
+            obj = Facility(name, surname, location, id, map_widget)
             facilities.append(obj)
             show_facilities()
+
+            # aktualizacja menu z placówkami
+            placowka_id_var.set("")
+            menu_placowka_id['menu'].delete(0, 'end')
+            for f in facilities:
+                menu_placowka_id['menu'].add_command(label=f.id, command=lambda value=f.id: placowka_id_var.set(value))
+
         elif typ == "Pracownik":
+            surname = entry_nazwisko.get()
             obj = Employee(name, surname, location, post, map_widget)
             employees.append(obj)
             show_employee()
         elif typ == "Klient":
+            surname = entry_nazwisko.get()
             obj = Client(name, surname, location, post, map_widget)
             clients.append(obj)
             show_clients()
@@ -105,6 +111,7 @@ def add_object():
     entry_miejscowosc.delete(0, END)
     entry_post.delete(0, END)
     typ_var.set("Placówka")
+
 
 
 def show_facilities() -> None:
@@ -124,12 +131,12 @@ def edit_facility():
     name = facilities[i].name
     surname = facilities[i].surname
     location = facilities[i].location
-    post = facilities[i].post
+    id = facilities[i].id
 
     entry_imie.insert(0, name)
     entry_nazwisko.insert(0, surname)
     entry_miejscowosc.insert(0, location)
-    entry_post.insert(0, post)
+    entry_post.insert(0, id)
 
     button_dodaj_obiekt.configure(text='Zapisz', command=lambda: update_facility(i))
 
@@ -137,12 +144,12 @@ def update_facility(i):
     name = entry_imie.get()
     surname = entry_nazwisko.get()
     location = entry_miejscowosc.get()
-    post = entry_post.get()
+    id = entry_post.get()
 
     facilities[i].name = name
     facilities[i].surname = surname
     facilities[i].location = location
-    facilities[i].post = post
+    facilities[i].id = id
 
     facilities[i].cordinates = facilities[i].get_cordinates()
     facilities[i].marker.delete()
@@ -178,12 +185,12 @@ def edit_employee() -> None:
     name = employees[i].name
     surname = employees[i].surname
     location = employees[i].location
-    post = employees[i].post
+    id_placowki = employees[i].id_placowki
 
     entry_imie.insert(0, name)
     entry_nazwisko.insert(0, surname)
     entry_miejscowosc.insert(0, location)
-    entry_post.insert(0, post)
+    entry_post.insert(0, id_placowki)
 
     button_dodaj_obiekt.configure(text='Zapisz', command=lambda: update_employee(i))
 
@@ -191,12 +198,12 @@ def update_employee(i):
     name = entry_imie.get()
     surname = entry_nazwisko.get()
     location = entry_miejscowosc.get()
-    post = entry_post.get()
+    id_placowki = entry_post.get()
 
     employees[i].name = name
     employees[i].surname = surname
     employees[i].location = location
-    employees[i].post = post
+    employees[i].id_placowki = id_placowki
 
     employees[i].cordinates = employees[i].get_cordinates()
     employees[i].marker.delete()
@@ -231,12 +238,12 @@ def edit_clients() -> None:
     name = clients[i].name
     surname = clients[i].surname
     location = clients[i].location
-    post = clients[i].post
+    id_placowki = clients[i].id_placowki
 
     entry_imie.insert(0, name)
     entry_nazwisko.insert(0, surname)
     entry_miejscowosc.insert(0, location)
-    entry_post.insert(0, post)
+    entry_post.insert(0, id_placowki)
 
     button_dodaj_obiekt.configure(text='Zapisz', command=lambda: update_client(i))
 
@@ -244,12 +251,12 @@ def update_client(i):
     name = entry_imie.get()
     surname = entry_nazwisko.get()
     location = entry_miejscowosc.get()
-    post = entry_post.get()
+    id_placowki = entry_post.get()
 
     clients[i].name = name
     clients[i].surname = surname
     clients[i].location = location
-    clients[i].post = post
+    clients[i].id_placowki = id_placowki
 
     clients[i].cordinates = clients[i].get_cordinates()
     clients[i].marker.delete()
@@ -284,6 +291,22 @@ def show_all_employees():
             text=f'{emp.name} {emp.surname} ({emp.location})'
         )
 
+def on_typ_change(*args):
+    typ = typ_var.get()
+    if typ == "Placówka":
+        Label_nazwisko.grid_remove()  # ukrywa etykietę "Nazwisko"
+        entry_nazwisko.grid_remove()  # ukrywa pole wpisywania nazwiska
+        Label_stanowisko.grid_remove()
+        entry_post.grid_remove()
+        menu_placowka_id.grid_remove()
+        Label_imie.config(text="Nazwa placówki:")
+    else:
+        Label_nazwisko.grid()         # pokazuje etykietę "Nazwisko"
+        entry_nazwisko.grid()         # pokazuje pole wpisywania nazwiska
+        Label_stanowisko.grid()
+        entry_post.grid()
+        menu_placowka_id.grid()
+        Label_imie.config(text="Imię:")
 
 #GUI
 root = Tk()
@@ -363,30 +386,41 @@ button_edytuj_klienta=Button(ramka_lista_klientow, text='Edytuj', command=edit_c
 button_edytuj_klienta.grid(row=2, column=2)
 
 #formularz dodawania
-Label(ramka_formularz, text="Imię:").grid(row=0, column=0)
+Label_imie = Label(ramka_formularz, text="Imię:")
+Label_imie.grid(row=0, column=0)
 entry_imie = Entry(ramka_formularz)
 entry_imie.grid(row=0, column=1)
 
-Label(ramka_formularz, text="Nazwisko:").grid(row=1, column=0)
+Label_nazwisko = Label(ramka_formularz, text="Nazwisko:")
+Label_nazwisko.grid(row=1, column=0)
 entry_nazwisko = Entry(ramka_formularz)
 entry_nazwisko.grid(row=1, column=1)
 
-Label(ramka_formularz, text="Miejscowość:").grid(row=2, column=0)
+Label(ramka_formularz, text="Miejscowość:").grid(row=0, column=2)
 entry_miejscowosc = Entry(ramka_formularz)
-entry_miejscowosc.grid(row=2, column=1)
+entry_miejscowosc.grid(row=0, column=3)
 
-Label(ramka_formularz, text="Stanowisko/Rola:").grid(row=3, column=0)
+Label_stanowisko = Label(ramka_formularz, text="Stanowisko/Rola:")
+Label_stanowisko.grid(row=1, column=2)
 entry_post = Entry(ramka_formularz)
-entry_post.grid(row=3, column=1)
+entry_post.grid(row=1, column=3)
 
 Label(ramka_formularz, text="Typ obiektu:").grid(row=4, column=0)
 typy = ["Placówka", "Pracownik", "Klient"]
 typ_var = StringVar(value=typy[0])
 menu_typu = OptionMenu(ramka_formularz, typ_var, *typy)
 menu_typu.grid(row=4, column=1)
+typ_var.trace_add('write', on_typ_change)
 
 button_dodaj_obiekt = Button(ramka_formularz, text="Dodaj", command=lambda: add_object())
-button_dodaj_obiekt.grid(row=5, column=0, columnspan=2, pady=10)
+button_dodaj_obiekt.grid(row=5, column=0, columnspan=4, pady=10)
+
+Label(ramka_formularz, text="Placówka ID:").grid(row=4, column=2)
+placowka_id_var = StringVar()
+menu_placowka_id = OptionMenu(ramka_formularz, placowka_id_var, "")
+menu_placowka_id.grid(row=4, column=3)
+
+
 
 #przyciski do map
 button_map_all_facilities = Button(ramka_mapa_przyciski, text="Mapa wszystkich placówek", command=show_all_facilities)
@@ -407,4 +441,5 @@ map_widget.grid(row=0, column=0, columnspan=2)
 map_widget.set_position(52.23,21.00)
 map_widget.set_zoom(6)
 
+on_typ_change()
 root.mainloop()
